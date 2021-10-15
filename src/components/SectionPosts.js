9051d275443c6c6d9ff284b537b680b23c14adc8
Stackbit Code Editor <projects@stackbit.com>
2021-10-15 04:35:18 +0000

    SectionPosts.js: changed

--- src/components/SectionPosts.js
+++ src/components/SectionPosts.js
@@ -28,7 +28,7 @@ export default class SectionPosts extends React.Component {
                 </div>
                 <div class="mb-1">
                               <Link key={index} href={postUrl} className="article-teaser flex">
-                                 <div class="onthird">
+                                 <div class="onethird">
                                     <img class="" src="https://cdn.pixabay.com/photo/2020/10/21/08/37/army-5672439__340.jpg" alt="" loading="lazy"/>
                                  </div>
                                  <div class="twothird p-1 w3-container">
