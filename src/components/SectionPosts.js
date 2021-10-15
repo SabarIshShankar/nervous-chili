bf290ed01154e2a89dc39393e64f80ce6722a45c
Stackbit Code Editor <projects@stackbit.com>
2021-10-15 03:59:44 +0000

    SectionPosts.js: changed

--- src/components/SectionPosts.js
+++ src/components/SectionPosts.js
@@ -27,6 +27,18 @@ export default class SectionPosts extends React.Component {
                     {excerpt && <p className="summary">{excerpt}</p>}
                     {hasMoreLink && moreLinkText && <div className="text-link">{moreLinkText}</div>}
                 </div>
+                <li class="mb-1">
+                              <a href="/article/What-is-happening-in-Afghanistan" class="flex">
+                                 <div class="w-1/3">
+                                    <img class="" src="https://cdn.pixabay.com/photo/2020/10/21/08/37/army-5672439__340.jpg" alt="" loading="lazy">
+                                 </div>
+                                 <div class="w-2/3 p-1 w3-container">
+                                    <h3 class="text-black text-base w3-hide-medium  m-px ">The Taliban situation, what's happening</h3>
+                                    <h1 class="text-black  mb-2 w3-hide-small bold text-xl">Who is responsible for the situation in Afghanistan?</h1>
+                                    <span class="text-xs text-gray-500 block mb-5">Aug 26 2021 <span class="w3-padding">Read more</span></span>
+                                 </div>
+                              </a>
+                           </li>
             </Link>
         );
     }
